bd52103a7b5503afe1e362ca45f06197061b0a66
Alicia Santiago <albrown07@mac.com>
2019-02-26 17:57:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,25 +6,25 @@ class Author
     @posts = []
   end 
  
-  def add_song(song)
-    self.songs << song
-    song.artist = self
-     @@song_count += 1
-  end
+  # def add_song(song)
+  #   self.songs << song
+  #   song.artist = self
+  #   @@song_count += 1
+  # end
    
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count += 1
-  end
+  # def add_song_by_name(name)
+  #   song = Song.new(name)
+  #   self.songs << song
+  #   song.artist = self
+  #   @@song_count += 1
+  # end
  
-  def songs
-    @songs
-  end
+  # def songs
+  #   @songs
+  # end
   
-  def self.song_count
-    @@song_count
-  end
+  # def self.song_count
+  #   @@song_count
+  # end
  
 end
