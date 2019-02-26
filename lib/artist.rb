f3264c7934ef849cfd828cb939bc89700aa07716
Alicia Santiago <albrown07@mac.com>
2019-02-26 17:50:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,19 +5,19 @@ class Artist
   def initialize(name)
      @name = name 
      @songs = []
-     @@song_count += 1
   end 
  
   def add_song(song)
     self.songs << song
     song.artist = self
-    # soung.count?
+     @@song_count += 1
   end
    
   def add_song_by_name(name)
     song = Song.new(name)
     self.songs << song
     song.artist = self
+    @@song_count += 1
   end
  
   def songs
