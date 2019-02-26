fe576a2b83f076d5ae1b3c18b1ca2fcb9b521bc9
Alicia Santiago <albrown07@mac.com>
2019-02-26 18:04:05 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,21 +3,21 @@ class Post
   
   @@all = []
   
-    # def initialize(name, artist = nil)
-    # @name = name
-    # @artist = artist 
-    # @@all << self
-    # end 
+    def initialize(title, author = nil)
+      @title = title
+      @author = author
+      @@all << self
+    end 
     
-    # def artist_name
-    #   if self.artist 
-    #     self.artist.name
-    #   else nil 
-    #   end
-    # end
+    def author_name
+      if self.artist 
+      self.artist.name
+       else nil 
+      end
+    end
   
-    # def self.all
-    #   @@all 
-    # end
+    def self.all
+      @@all 
+    end
     
 end
