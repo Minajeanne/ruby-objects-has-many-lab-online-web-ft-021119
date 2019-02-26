b9ff4892c6541cb82fa830b4b446ff6d0b9273d9
Alicia Santiago <albrown07@mac.com>
2019-02-26 17:40:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,8 +12,8 @@ class Artist
     # soung.count?
   end
    
-  def add_song_by_name(name, genre)
-    song = Song.new(name, genre)
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
     @songs << song
     song.artist = self
   end
